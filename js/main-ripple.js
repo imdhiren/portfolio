dcad949952352cbf62d331bfcf64aacc8f68a5c3
Dhiren Patel <dhirenspatel@hotmail.com>
2023-04-07 00:25:14 +0530

    theme make in react js initial check-in
    theme make in react js initial check-in

--- js/main-ripple.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-
-	"use strict";
-
-	jQuery("#ripple").ripples({ resolution: 500, dropRadius: 20, perturbance: 0.04 });
-	jQuery("#ripple-two").ripples({ resolution: 500, dropRadius: 20, perturbance: 0.04 });
-
-});
